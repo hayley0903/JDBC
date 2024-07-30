783a056c59b3138b0dda93765307a7ce390256b2
hayley0903 <hyejeonglee0903@gmail.com>
2024-07-30 12:28:55 +0900

    <2024.07.30>
    도서관리수정 및 삭제

--- JDBC_test/src/book/BookDAO.java
+++ JDBC_test/src/book/BookDAO.java
@@ -24,11 +24,12 @@ public class BookDAO {
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 
-	//정상적으로 실행시(데이터의 변동이 있을시) 반환값은 1//데이터 변동이 없을시 반환값은 0
+	//정상적으로 실행시(데이터의 변동이 있을시) 데이터 변화가 한번일때 1, 세번일때 3//데이터 변동이 없을시 반환값은 0
 	public int insertBook(BookInfo bookInfo) {
 
 		int resultChk = 0;
 
+		//DB접속
 		try {
 			Class.forName(driver);
 			conn = DriverManager.getConnection(db_url, "root", "1234");
@@ -44,7 +45,7 @@ public class BookDAO {
 			e.printStackTrace();
 		}
 
-		//쿼리실행
+		//접속 후 쿼리실행 --> 도서정보등록
 		try {
 			String sql = "INSERT INTO `jdbc_test`.`tb_book_info`\r\n"
 					+ "(\r\n"
@@ -73,17 +74,16 @@ public class BookDAO {
 			pstmt.setInt(7, bookInfo.getPage());
 
 
-			//결과를 담을때 select문의 경우 executeQuery();를 사용하고
+			//결과를 담을때 select문의 경우 (조회만 할떄는) executeQuery();를 사용
 			//rs = pstmt.executeQuery();
 
-			//insert문일경우 executeUpdate()를 사용하여 결과를 담음;
+			//insert문일경우 executeUpdate()를 사용하여 결과를 담고 실행
 			resultChk = pstmt.executeUpdate();
 
 		}catch (SQLException e) {
 			System.out.println("error :" + e);
 		}finally {
 			try {
-
 				if(pstmt != null) {
 					pstmt.close();
 				}
@@ -249,12 +249,167 @@ public class BookDAO {
 			}
 		}
 		return bookList;
-
-
-
-
 	}
 
 
+	public int removeBook(String title) {
 
+		int resultChk = 0;
+
+		//DB접속
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(db_url, "root", "1234");
+			if(conn != null) {
+				System.out.println("접속완료");
+			}
+		}catch(ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+
+		}catch(SQLException e) {
+			System.out.println("DB접속 실패");
+			e.printStackTrace();
+		}
+
+		//접속 후 쿼리실행 --> 도서정보등록
+		try {
+			String sql = "delete from tb_book_info where book_title = ?;";
+
+
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, title);
+
+
+			//insert문일경우 executeUpdate()를 사용하여 결과를 담고 실행
+			resultChk = pstmt.executeUpdate();
+
+		}catch (SQLException e) {
+			System.out.println("error :" + e);
+		}finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null && conn.isClosed()) {
+					conn.close();
+				}
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return resultChk;
+	}
+	
+	public int removeOneBook(int bookId) {
+
+		int updateChk = 0;
+
+		//DB접속
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(db_url, "root", "1234");
+			if(conn != null) {
+				System.out.println("접속완료");
+			}
+		}catch(ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+
+		}catch(SQLException e) {
+			System.out.println("DB접속 실패");
+			e.printStackTrace();
+		}
+
+		//접속 후 쿼리실행 --> 도서정보등록
+		try {
+			String sql = "delete from tb_book_info where book_id = ?;";
+
+
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, bookId);
+
+
+			//insert문일경우 executeUpdate()를 사용하여 결과를 담고 실행
+			updateChk = pstmt.executeUpdate();
+
+		}catch (SQLException e) {
+			System.out.println("error :" + e);
+		}finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null && conn.isClosed()) {
+					conn.close();
+				}
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return updateChk;
+	}
+
+	public int updateBook(int bookId, String updateTitle) {
+
+		int updateChk = 0;
+
+		//DB접속
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(db_url, "root", "1234");
+			if(conn != null) {
+				System.out.println("접속완료");
+			}
+		}catch(ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+
+		}catch(SQLException e) {
+			System.out.println("DB접속 실패");
+			e.printStackTrace();
+		}
+
+		//접속 후 쿼리실행 --> 도서정보등록
+				try {
+					String sql = "UPDATE tb_book_info\r\n"
+							+ "SET book_title = ?\r\n"
+							+ "WHERE book_id = ?;";
+
+
+					pstmt = conn.prepareStatement(sql);
+
+					pstmt.setString(1, updateTitle);
+					pstmt.setInt(2, bookId);
+
+					//결과를 담을때 select문의 경우 (조회만 할떄는) executeQuery();를 사용
+					//rs = pstmt.executeQuery();
+
+					//insert문일경우 executeUpdate()를 사용하여 결과를 담고 실행
+					updateChk = pstmt.executeUpdate();
+
+		}catch (SQLException e) {
+			System.out.println("error :" + e);
+		}finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null && conn.isClosed()) {
+					conn.close();
+				}
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return updateChk;
+	}
+	
+	
+	
 }
--- JDBC_test/src/book/BookServiceImpl.java
+++ JDBC_test/src/book/BookServiceImpl.java
@@ -8,12 +8,12 @@ import java.util.Scanner;
 public class BookServiceImpl implements BookService {
 
 	Scanner sc = new Scanner(System.in);
-	
+
 	//BookDAO와 연결하기
 	BookDAO bookDAO = new BookDAO();
-	
-	
-	
+
+
+
 	@Override
 	public void startProgram() {
 
@@ -31,11 +31,11 @@ public class BookServiceImpl implements BookService {
 				break;
 			case 2 : 
 				System.out.println("2. 도서 정보 수정");
-				//updateMember();
+				updateBook();
 				break;
 			case 3 : 
 				System.out.println("3. 도서 정보 삭제");
-				//removeMember();
+				removeOneBook();
 				break;
 			case 4 : 
 				System.out.println("4. 도서 정보 출력(이름)");
@@ -57,7 +57,7 @@ public class BookServiceImpl implements BookService {
 
 	@Override
 	public int printMenu() {
-	
+
 		System.out.println("===== 도서 관리 프로그램 =====");
 		System.out.println("1. 도서 정보 등록");
 		System.out.println("2. 도서 정보 수정");
@@ -70,11 +70,11 @@ public class BookServiceImpl implements BookService {
 		int choice = sc.nextInt();
 		return choice;
 	}
-	
+
 	//"1. 도서 정보 등록"
 	public void insertBook() {
-		
-	
+
+
 		System.out.println("도서명을 입력하세요");
 		sc.nextLine();
 		String title = sc.nextLine();
@@ -91,10 +91,10 @@ public class BookServiceImpl implements BookService {
 		String isbn = sc.nextLine();
 		System.out.print("총페이지을 입력하세요");
 		int page = sc.nextInt();
-		
+
 		//입력값을 bookInfo에 넣기
 		BookInfo bookInfo = new BookInfo();
-		
+
 		bookInfo.setTitle(title);
 		bookInfo.setPrice(price);
 		bookInfo.setAuthor(author);
@@ -102,7 +102,7 @@ public class BookServiceImpl implements BookService {
 		bookInfo.setPubYear(pubYear);
 		bookInfo.setIsbn(isbn);
 		bookInfo.setPage(page);
-		
+
 		//bookInfo에 담은 값을 bookDAO에 넣기
 		int resultChk = 0;
 		resultChk = bookDAO.insertBook(bookInfo);
@@ -110,58 +110,169 @@ public class BookServiceImpl implements BookService {
 			System.out.println("도서 등록 완료");
 		} else {System.out.println("도서 등록 실패");}
 	}
-	
+
 	//"5. 도서 전체 정보 출력"
-	public void printAllBooks(){
-		List<HashMap<String, Object>> bookList = new ArrayList();	
+	public void printAllBooks() {
+		List<HashMap<String, Object>> bookList = new ArrayList();
 		bookList = bookDAO.printAllBooks();
-		
-		System.out.println("도서명\t저자\t출판사\\t출판년도\\tISBN\\t페이지");
-		
-		for(int i = 0; i<bookList.size();i++) {
-			System.out.println(bookList.get(i).get("book_title")+"\t");
-			System.out.println(bookList.get(i).get("book_author")+"\t");
-			System.out.println(bookList.get(i).get("book_publisher")+"\t");
-			System.out.println(bookList.get(i).get("book_pubYear")+"\t");
-			System.out.println(bookList.get(i).get("book_isbn")+"\t");
-			System.out.println(bookList.get(i).get("book_page")+"\t");
-		
+		System.out.println("도서ID\t도서명\t가격\t저자\t출판사\t출판년도\tISBN\t등록일");
+		for(int i =0; i<bookList.size(); i++) {
+			System.out.print(bookList.get(i).get("book_Id")+"\t");
+			System.out.print(bookList.get(i).get("book_title")+"\t");
+			System.out.print(bookList.get(i).get("book_price")+"\t");
+			System.out.print(bookList.get(i).get("book_author")+"\t"); 
+			System.out.print(bookList.get(i).get("book_publisher")+"\t");
+			System.out.print(bookList.get(i).get("book_pubYear")+"\t");
+			System.out.print(bookList.get(i).get("book_isbn")+"\t");
+			System.out.println(bookList.get(i).get("create_date"));
 		}
 	}
-	
+
 	//"4. 도서 정보 출력(이름)"
 	public void printBook() {
 		List<HashMap<String, Object>> bookList = new ArrayList();
-		
-		
-		System.out.println("검색할 도서명를 입력하세요 : ");
+		System.out.println("검색할 도서명을 입력하세요");
+		String title = sc.next();
+
+		bookList = bookDAO.printBook(title);
+
+		System.out.println("도서ID\t도서명\t가격\t저자\t출판사\t출판년도\tISBN\t등록일");
+		for(int i =0; i<bookList.size(); i++) {
+			System.out.print(bookList.get(i).get("book_Id")+"\t");
+			System.out.print(bookList.get(i).get("book_title")+"\t");
+			System.out.print(bookList.get(i).get("book_price")+"\t");
+			System.out.print(bookList.get(i).get("book_author")+"\t"); 
+			System.out.print(bookList.get(i).get("book_publisher")+"\t");
+			System.out.print(bookList.get(i).get("book_pubYear")+"\t");
+			System.out.print(bookList.get(i).get("book_isbn")+"\t");
+			System.out.println(bookList.get(i).get("create_date"));
+		}
+
+	}
+	
+
+	//"3. 도서 정보 삭제"
+	public void removeBook() {
+		List<HashMap<String, Object>> bookList = new ArrayList();
+		System.out.println("검색할 도서명을 입력하세요");
 		String title = sc.next();
 		bookList = bookDAO.printBook(title);
 		
-		System.out.println("도서명\t저자\t출판사\t출판년도\tISBN\t페이지");
-		
-		boolean flag = false;
-		
-		for(int i = 0; i<bookList.size();i++) {
-			System.out.println(bookList.get(i).get("book_title")+"\t");
-			System.out.println(bookList.get(i).get("book_author")+"\t");
-			System.out.println(bookList.get(i).get("book_publisher")+"\t");
-			System.out.println(bookList.get(i).get("book_pubYear")+"\t");
-			System.out.println(bookList.get(i).get("book_isbn")+"\t");
-			System.out.println(bookList.get(i).get("book_page")+"\t");
-		
-		}
-		
-		if (flag ==false) {
-			System.out.println("등록된 도서가 없음");
-		}
-		
-		
+		int resultChk = 0;
+		resultChk = bookDAO.removeBook(title);
+		if(resultChk > 0 ) {
+			System.out.println("도서 삭제 완료");
+		} else {System.out.println("도서 삭제 실패");}
 		
 	}
 	
-	
-	
+	//"3. 도서 정보 선택 삭제"
+		public void removeOneBook() {
+			List<HashMap<String, Object>> bookList = new ArrayList();
+			System.out.println("도서명을 입력하세요");
+			String title = sc.next();
+			bookList = bookDAO.printBook(title);
+			
+			System.out.println("도서ID\t도서명\t가격\t저자\t출판사\t출판년도\tISBN\t등록일");
+			for(int i =0; i<bookList.size(); i++) {
+				System.out.print(bookList.get(i).get("book_Id")+"\t");
+				System.out.print(bookList.get(i).get("book_title")+"\t");
+				System.out.print(bookList.get(i).get("book_price")+"\t");
+				System.out.print(bookList.get(i).get("book_author")+"\t"); 
+				System.out.print(bookList.get(i).get("book_publisher")+"\t");
+				System.out.print(bookList.get(i).get("book_pubYear")+"\t");
+				System.out.print(bookList.get(i).get("book_isbn")+"\t");
+				System.out.println(bookList.get(i).get("create_date"));}
+			
+			System.out.println("삭제할 도서의 순번을 입력하세요");
+			int num = sc.nextInt();
+			//HashMap List에서 도서 순번을 호출하기
+			//bookList.get(num-1).get("book_id")은 object형이므로 int형으로 바꾸기
+			int bookId = (int) bookList.get(num-1).get("book_Id");
+			
+			int updateChk = 0;
+			updateChk = bookDAO.removeOneBook(bookId);
+			if(updateChk > 0 ) {
+				System.out.println("도서 삭제 완료");
+			} else {System.out.println("도서 삭제 실패");}
+			
+		}
 	
 
+	//"2. 도서 정보 수정"//중복데이터 선택하여 수정//
+		public void updateBook() {
+
+			System.out.println("수정할 도서명을 입력하세요");
+			String title = sc.next();
+			
+			List<HashMap<String, Object>> bookList = new ArrayList();
+			bookList = bookDAO.printBook(title);
+			System.out.println("도서ID\t도서명\t가격\t저자\t출판사\t출판년도\tISBN\t등록일");
+			for(int i =0; i<bookList.size(); i++) {
+				System.out.print(bookList.get(i).get("book_Id")+"\t");
+				System.out.print(bookList.get(i).get("book_title")+"\t");
+				System.out.print(bookList.get(i).get("book_price")+"\t");
+				System.out.print(bookList.get(i).get("book_author")+"\t"); 
+				System.out.print(bookList.get(i).get("book_publisher")+"\t");
+				System.out.print(bookList.get(i).get("book_pubYear")+"\t");
+				System.out.print(bookList.get(i).get("book_isbn")+"\t");
+				System.out.println(bookList.get(i).get("create_date"));
+				}
+			System.out.println("수정할 도서의 순번을 입력하세요");
+			int num = sc.nextInt();
+			
+			//string --> int형으로 변환
+			int bookId = Integer.parseInt(bookList.get(num-1).get("book_Id").toString()) ;
+			
+			
+			System.out.println("수정할 도서명을 입력하세요");
+			String updateTitle = sc.next();
+			
+			int updateChk = 0;
+			updateChk = bookDAO.updateBook(bookId, updateTitle);
+			if(updateChk > 0 ) {
+				System.out.println("도서 등록 완료");
+			} else {System.out.println("도서 등록 실패");}
+		}
+		
+		
+		
+		//"2. 도서 정보 수정"
+//		public void updateBook() {
+//			List<HashMap<String, Object>> bookList = new ArrayList();
+//			
+//			System.out.println("수정할 도서명을 입력하세요");
+//			String title = sc.next();
+//			bookList = bookDAO.printBook(title);
+//			
+//			
+//			BookInfo bookInfo = new BookInfo();
+//			
+//			System.out.print("수정할 도서가격을 입력하세요");
+//			bookInfo.setPrice(sc.nextInt());
+//			
+//			System.out.print("수정할 저자명을 입력하세요");
+//			bookInfo.setAuthor(sc.next());
+//			
+//			System.out.print("수정할 출판사를 입력하세요");
+//			bookInfo.setPublisher(sc.next());
+//			
+//			System.out.print("수정할 출판년도를 입력하세요");
+//			bookInfo.setPubYear(sc.next());
+//			
+//			System.out.print("수정할 ISBN을 입력하세요");
+//			bookInfo.setIsbn(sc.next());
+//			
+//			System.out.print("수정할 총페이지을 입력하세요");
+//			bookInfo.setPage(sc.nextInt());
+//			
+//			int updateChk = 0;
+//			updateChk = bookDAO.updateBook(title, bookInfo);
+//			if(updateChk > 0 ) {
+//				System.out.println("도서 등록 완료");
+//			} else {System.out.println("도서 등록 실패");}
+//			
+//		}
+	//	
+			
 }
